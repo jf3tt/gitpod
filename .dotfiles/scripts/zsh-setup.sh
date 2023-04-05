78cf7131ab47471d416d61451270538d42924e84
Rodion Nosankov <geforce.exe@gmail.com>
2023-04-05 17:45:17 +0000

    update

--- .dotfiles/scripts/zsh-setup.sh
+++ .dotfiles/scripts/zsh-setup.sh
@@ -3,7 +3,7 @@
 mkdir -p "/home/gitpod/.zsh" 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "/home/gitpod/.zsh/themes/spaceship-prompt" --depth=1
-ln -s "/home/gitpod/.zsh/themes/spaceship-prompt/spaceship.zsh-theme" "/home/gitpod/.zsh/themes/spaceship.zsh-theme"
+mkdir -p "/home/gitpod/.zsh"
+git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git "/home/gitpod/.zsh/spaceship"
 
 cp /home/gitpod/.config/.zshrc ~/.zshrc
